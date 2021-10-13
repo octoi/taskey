be16b51acd144f937c9c924857f159e19075ec60
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-13 20:05:00 +0530

    feature: check

--- app/lib/widgets/todo_list.dart
+++ app/lib/widgets/todo_list.dart
@@ -13,14 +13,45 @@ class TodoList extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: List.generate(20, (index) {
-            return Todo(
-              id: index.toString(),
-              key: ValueKey(index),
-              text: 'Design & develop this app',
-              isResolved: index % 2 == 0,
-            );
+            return TodoItem(index: index);
           }),
         ),
+      ),
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final int index;
+  const TodoItem({Key? key, required this.index}) : super(key: key);
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  // bool isResolved = 10 % 2 == 0;
+  late bool isResolved;
+
+  @override
+  void initState() {
+    super.initState();
+    isResolved = widget.index % 2 == 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isResolved = !isResolved;
+        });
+      },
+      child: Todo(
+        id: widget.index.toString(),
+        key: ValueKey(widget.index),
+        text: 'Design & develop this app',
+        isResolved: isResolved,
       ),
     );
   }
